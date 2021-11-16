01995c9fac2d96e476b4ad6c6e94047a4729c072
Axel Lopez <josaphet.cruz@unah.hn>
2021-11-15 22:36:48 -0600

    se creo archivo de funciones en R

---
+++ funciones.R
@@ -0,0 +1,9 @@
+#trabajando con funciones 
+# conocer la estructura de la funcion 
+#function(){}   (objecto){instrucciones}
+suma <- function(x,y){ 
+  adicion <- x+y 
+  resta <- x-y
+  print(adicion)
+  print(resta)
+}
