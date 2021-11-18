884eadfd91690c5fea0d9cda2e5304c2f8217d48
Axel Lopez <josaphet.cruz@unah.hn>
2021-11-17 20:16:54 -0600

    se agrego dos archivos funciones.R y funciones.Rnw

---
+++ Funciones.R
@@ -0,0 +1,13 @@
+#trabajando con funciones 
+# conocer la estructura de la funcion 
+#function(){}   (objecto){instrucciones}
+suma <- function(x,y){ 
+  adicion <- x+y 
+  resta <- x-y
+  multiplicacion <- x*y
+  print(adicion)
+  print(resta)
+  print(multiplicacion)
+}
+suma(2,5)
+suma(5,2)
